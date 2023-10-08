e9bb2a0f81daadddbfec0be38c116318fa67d77d
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-10-07 21:19:31 -0500

    agrega condiciones para las entradas

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ var eliminados = []; // Array con los cuadros eliminados
 const velocidad = 20; // Velocidad de movimiento
 const cuadroSize = 50; // Tamaño de cada cuadro
 const suelo = canvasIndex.height - 2; // Posición del suelo
-var espaciado = suelo;
-var n = 0;
-var contador = 0;
-const posicionXInicial = 25;
-const posicionYInicial = 10;
+var espaciado = suelo; // Posicion del suelo modificable
+var n = 0; // Numero de letras b que entran
+var contador = 0; // Numero de letras que entran
+const posicionXInicial = 25; // Posición en X del cuadro
+const posicionYInicial = 10; // Posición en Y del cuadro
+
+var contadorEntradas = 0; // Para validar las entradas
+var invalida = false; // Poner entradas como validas o invalidas
+
+var pEntrada = document.getElementById("p-entrada"); // Parrafo donde se muestra la entrada completa
+var pEstado = document.getElementById("p-estado"); // Parrafo donde se muestra el estado de la entrada
 
 
-var pEntrada = document.getElementById("p-entrada");
-var pEstado = document.getElementById("p-estado");
 
-//Agrega un cuadro al vector de cuadros
+
+// Agrega un cuadro al vector de cuadros
 function agregarCuadro(colorC, letraC, espaciadoC, posicionC){
     cuadros.push({
         x: posicionXInicial,
@@ -29,13 +34,20 @@ function agregarCuadro(colorC, letraC, espaciadoC, posicionC){
         posicion: posicionC,
     });
     contador = contador + 1;
-    espaciado = espaciado - cuadroSize;
+    espaciado = espaciado - cuadroSize; // Cambia la posicion donde se pondrá el siguiente cuadro
 }
 
+
+
+
+// Actualiza el parrafo del INDEX con la entrada completa
 function actualizaEntrada (entrada){
     let anterior = pEntrada.textContent;
     pEntrada.textContent = anterior + entrada;
 }
+
+
+
 
 // Agregar un evento para detectar las teclas presionadas
 window.addEventListener('keydown', function (event) {
@@ -46,26 +58,88 @@ window.addEventListener('keydown', function (event) {
         agregarCuadro("darkgoldenrod", "a", espaciado - cuadroSize, contador);
         actualizaEntrada("a");
 
+        if(contadorEntradas !== 0){
+
+            pEstado.textContent = "Invalida";
+            invalida = true;
+
+        }else{
+
+            contadorEntradas = contadorEntradas + 1;
+        }
+
+
     }else if(event.key === "b"){
+
         //rgb(34, 177, 76) #6b8e23
         agregarCuadro("darkcyan", "b", espaciado - cuadroSize, contador);
-        n = n + 1;
         actualizaEntrada("b");
+
+        if(invalida=== false){
+            n = n + 1;
+        }
+
+        if(contadorEntradas !== 1){
+            pEstado.textContent = "Invalida";
+            invalida = true;
+        }
+        
 
     }else if(event.key === "c"){
 
         agregarCuadro("orangered", "c", espaciado - cuadroSize, contador);
-        espaciado = espaciado + cuadroSize*2;
         actualizaEntrada("c");
+
+        espaciado = espaciado + cuadroSize*2;
+        contadorEntradas = contadorEntradas + 1;
+
+        if(contadorEntradas !== 2){
+
+            pEstado.textContent = "Invalida";
+            invalida = true;
+
+        }else{
+
+            contadorEntradas = contadorEntradas + 1;
+
+            if(n === 0 && invalida === false){
+
+                pEstado.textContent = "Aceptada";
+                contadorEntradas = 4;
+            }
+           
+        }
+
 
     }else if(event.key === "d"){
 
         agregarCuadro("orange", "d", espaciado - cuadroSize, contador);
-        espaciado = espaciado + cuadroSize*2;
         actualizaEntrada("d");
+
+        espaciado = espaciado + cuadroSize*2;
+
+        if(invalida=== false){
+            n = n - 1;
+        }
+
+        if(contadorEntradas !== 3){
+            pEstado.textContent = "Invalida";
+            invalida = true;
+        }
+
+        if(n === 0 && invalida === false){
+
+            pEstado.textContent = "Aceptada";
+            contadorEntradas = contadorEntradas + 1;
+
+        }else{
+            
+            pEstado.textContent = "Invalida";
+        }
     }
 
 });
+
 
 
 
@@ -93,6 +167,7 @@ function dibujar(){
         canvas.strokeRect(i.x, i.y, i.width, i.height); // Dibuja el borde alrededor del cuadro existente
 
         if(i.y >= i.espaciado && i.mover === true){
+
             i.y = i.espaciado;
             i.mover = false;
             if(i.letra === "c" || i.letra === "d"){
@@ -104,10 +179,14 @@ function dibujar(){
             }
 
         }else if(i.mover === true){
+
             i.y = i.y + velocidad;
         }
 
     });
+
+
+
 
     // Anima eliminación de cuadros
     eliminados.forEach(function(i){
@@ -123,16 +202,23 @@ function dibujar(){
         canvas.fillStyle = 'white'; // Color del texto
         canvas.font = '30px Arial'; // Fuente y tamaño del texto
         canvas.fillText(i.letra, i.x + 20, i.y + 35); // Dibujar la letra en el centro del cuadro
+
         if(i.width >= 2){
+
             i.width = i.width - 4;
             i.height = i.height - 4;
             i.x = i.x + 2;
             i.y = i.y + 2;
+
         }else{
             eliminados.pop();
         }
+
     });
 }
+
+
+
 
 function gameLoop(){
     dibujar();
